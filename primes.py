56deade12aefe125263c3134dadcb16c426b9b01
Ricky Brown <ricky.brown@kcl.ac.uk>
2022-10-06 18:30:49 +0100

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
+import re
+
+
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def isPrime(n):
+    # Use regex function to find n primes.
+    return re.match(r'^1?$|^(11+?)\1+$', '1' * n) == None
+
+
 def primes(number_of_primes):
     list = []
-    return list
+
+    while len(list) < number_of_primes:
+        upper = 100
+        # Add n primes to our list.
+        list += filter(isPrime, range(upper - 100, upper))
+        upper += 100
+
+    # Cap the number of primes shown to "number_of_primes"
+    return list[:number_of_primes]
